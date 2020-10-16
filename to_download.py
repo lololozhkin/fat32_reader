135b72262be72bee9a04a5d9af980157c634cd44
lololozhkin <alexand3r.lozhkin@gmail.com>
2020-10-16 15:05:57 +0500

    beated image added

--- to_download.py
+++ to_download.py
@@ -1,5 +1,6 @@
 FILES = {
-    "https://yadi.sk/d/SuC2BVuGUGA-tg": 'simple_image_only_eng_letters.img'
-    "https://yadi.sk/d/aAnGMhXa0dc-pw": 'russian_and_english_letters.img'
+    "https://yadi.sk/d/SuC2BVuGUGA-tg": 'simple_image_only_eng_letters.img',
+    "https://yadi.sk/d/aAnGMhXa0dc-pw": 'russian_and_english_letters.img',
+    "https://yadi.sk/d/y2XjQEBM1ymxdQ": 'beated_image.img'
 }
 
